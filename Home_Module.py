daedf55a1cae0dcbf92f978fbfcf87d46a64a986
Hezron Na <hezron.w12@gmail.com>
2023-11-29 20:36:19 +0300

    Committed 2023 1We.203619

--- Home_Module.py
+++ Home_Module.py
@@ -91,7 +91,7 @@ def Homepage_Background(widget):
         while True:
             if x == 1:
                 widget.config(image=image1)
-                imag.
+                widget.image = image1
                 x = 2
             elif x == 2:
                 widget.config(image=image2)
